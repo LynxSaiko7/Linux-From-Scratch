639ea6e527099a148188002d1dd29d2e5e845771
LynxSaiko7 <lynxsaiko7@gmail.com>
2026-04-17 03:21:00 +0700

    Delete push.sh

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git remote set-url origin https://github.com/LynxSaiko7/Linux-From-Scratch.git
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/LynxSaiko7/Linux-From-Scratch.git
-git push -u origin main
